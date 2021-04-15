69584da2c442ee15576f55b54eef2beefa59ef3f
Terrnox <81361890+Terrnox@users.noreply.github.com>
2021-04-15 13:22:23 +0200

    Add files via upload

--- npy.py
+++ npy.py
@@ -16,20 +16,36 @@ twb_P = 'D'
 wwb_L = 0
 wwb_P = 1
 
-x0=0
+x0=1
 xp=2
-n=100
+n=5
 
-Twe = np.zeros((n,3))
-print(Twe)
-#def GenTabGeo(x0,xp,n):
-   # np.linspace(x0,xp,n)
-    #for i in range(n):        
-    #return Twe,Tel
+def GenTabGeo(x0,xp,n):
+   tem = (xp - x0)/(n-1)
+   TabGeo = np.array([0,0 *tem+x0])
+   for i in range(n): 
+          TabGeo = np.block([
+              [TabGeo],
+              [i, i*tem+x0],])
+   TabGeo = np.delete(TabGeo,1,0)
+   return TabGeo
 
-#def GeoShow():
- #   plt.plot(Twe,Tel)
- #   plt.show()
+print(GenTabGeo(x0,xp,n))
+
+def generujTabliceGeometrii(x_0, x_p, n):
+    temp = (x_p - x_0) / (n - 1)
+    matrix = np.array([x_0])
+
+    for i in range(1, n, 1):
+        matrix = np.block([matrix, i * temp + x_0])
+    return matrix
+
+print(generujTabliceGeometrii(1,2,5))
+#def GeoShow(TabGeo):
+#   np.array(TabGeo)
+#   np.array(TabGeo)
+#   plt.plot()
+#   plt.show()
     
 #def AloMem(n):
 #    return A,b
