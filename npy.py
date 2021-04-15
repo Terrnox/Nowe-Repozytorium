5e6dd4f224bccdc20bbbfb0f9aae57c95e280a31
Terrnox <81361890+Terrnox@users.noreply.github.com>
2021-04-15 16:09:22 +0200

    Add files via upload

--- npy.py
+++ npy.py
@@ -1,6 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def GenTabGeo(x0,xp,n):
+   tem = (xp - x0)/(n-1)
+   TabGeo = np.array([0,0 *tem+x0])
+   for i in range(n): 
+          TabGeo = np.block([
+              [TabGeo],
+              [i, i*tem+x0],])
+   TabGeo = np.delete(TabGeo,1,0)
+   return TabGeo
+
+def GeoShow(TabGeo,n):
+   
+   x = np.zeros((n,1))
+   points = TabGeo[:,1]
+   plt.plot(points,x,'|',points,x)
+   plt.show()
+  
 Twe = np.array([[1, 0],
                [2, 1],
                [3, 0.5],
@@ -16,29 +33,12 @@ twb_P = 'D'
 wwb_L = 0
 wwb_P = 1
 
-
-def GenTabGeo(x0,xp,n):
-   tem = (xp - x0)/(n-1)
-   TabGeo = np.array([0,0 *tem+x0])
-   for i in range(n): 
-          TabGeo = np.block([
-              [TabGeo],
-              [i, i*tem+x0],])
-   TabGeo = np.delete(TabGeo,1,0)
-   return TabGeo
-  
-
-def GeoShow(TabGeo,n):
-   x=TabGeo[:,1]
-   y=np.zeros((n,1))
-   plt.plot(x,y)
-   plt.show()
-
-x0=1
-xp=2
+x0=0
+xp=1
 n=5
 
 TabGeo = GenTabGeo(x0,xp,n) 
-GeoShow(TabGeo,n)   
+GeoShow(TabGeo,n) 
+  
 #def AloMem(n):
 #    return A,b
